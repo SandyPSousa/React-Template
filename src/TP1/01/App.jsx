6ae948d19722a696d0d04ef94a683c7ce342deed
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-01 18:06:06 -0300

    exercício 1 - ok

--- src/TP1/01/App.jsx
+++ src/TP1/01/App.jsx
@@ -1,5 +1,25 @@
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return (
+    <div class="container">
+      <div className="menu">
+        <img
+          className="menu-img"
+          src="https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png"
+        />
+        <p>Brand</p>
+      </div>
+
+      <nav>
+        <ul>
+          <li>Serviços</li>
+          <li>Contato</li>
+          <li>Sobre</li>
+        </ul>
+      </nav>
+      <img
+        className="profile-img"
+        src="https://freesvg.org/img/abstract-user-flat-4.png"
+      />
+    </div>
+  );
 }
