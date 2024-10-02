c3bc30474683f3e99ec5a91c21d7c265f618c93d
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-02 17:08:14 -0300

    exercicio 04 ok

--- src/TP1/04/App.jsx
+++ src/TP1/04/App.jsx
@@ -1,5 +1,28 @@
+import style from "./container.module.css"
+   
+
+
 export default function App() {
-    return (
-        <div>Hello World</div>
-    )
+  return (
+    <div className={style.container}>
+      <div className={style.header}>
+        <h1>Header</h1>
+      </div>
+      <div className={style.globalMenu}>
+        <h1>Global Menu</h1>
+      </div>
+      <div className={style.contextMenu}>
+        <h1>Context Menu</h1>
+      </div>
+      <div className={style.mainContent}>
+        <h1>Main Content</h1>
+      </div>
+      <div className={style.ads}>
+        <h1>Ads</h1>
+      </div>
+      <div className={style.footer}>
+        <h1>Footer</h1>
+      </div>
+    </div>
+  );
 }
