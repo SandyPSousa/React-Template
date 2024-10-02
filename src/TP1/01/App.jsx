2b0b2080c6a53a298ae4fd7f5a8b4c9eebf16867
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-02 12:28:58 -0300

    exercício 01 alteração logo img

--- src/TP1/01/App.jsx
+++ src/TP1/01/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
       <div className="menu-icon">
         <img
           className="menu-img"
-          src="https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png"
+          src="https://img.freepik.com/free-photo/realistic-s-letter-with-soft-texture_23-2150458467.jpg?t=st=1727882878~exp=1727886478~hmac=67dc83a06424e1b4cae22d3e42f14fd992b6b1710bc637fd481084981686d6b4&w=740"
         />
         <p>Brand</p>
       </div>
