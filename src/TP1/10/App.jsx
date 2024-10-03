a2060173b198183db8b9bc8fef473381511c8f6c
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-03 15:35:57 -0300

    exercicio 10 ok

--- src/TP1/10/App.jsx
+++ src/TP1/10/App.jsx
@@ -1,5 +1,43 @@
+import React, { useState } from "react";
+import styles from "./style.module.css";
+
 export default function App() {
-    return (
-        <div>Faça você mesmo!</div>
-    )
+  const [expanded, setExpanded] = useState(false);
+  const [hideToggle, setHideToggle] = useState(false);
+  const handleToggleClick = () => {
+    setExpanded(!expanded);
+    setHideToggle(true);
+  };
+
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.menuHeader}>
+        <div
+          className={hideToggle ? styles.hideToggle : styles.menuToggle}
+          onClick={handleToggleClick}
+        >
+          <img
+            className={styles.img}
+            src="https://cdn-icons-png.flaticon.com/512/2311/2311531.png"
+          />
+        </div>
+        <div className={styles.platformBrand}>Brand</div>
+        <div className={styles.profileIcon}>
+          <img
+            className={styles.img}
+            src="https://freesvg.org/img/abstract-user-flat-4.png"
+          />
+        </div>
+      </div>
+
+      {expanded && (
+        <div className={styles.menuItems}>
+          <div className={styles.menuItem}>Home</div>
+          <div className={styles.menuItem}>About</div>
+          <div className={styles.menuItem}>Services</div>
+          <div className={styles.menuItem}>Contact</div>
+        </div>
+      )}
+    </div>
+  );
 }
