c8c5ed05b0619f0efd1d239e14c03cc2ea9a9c16
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-04 12:23:43 -0300

    exercicio 15 ok

--- src/TP1/15/components/Post.jsx
+++ src/TP1/15/components/Post.jsx
@@ -1,36 +1,3 @@
-// import React from 'react';
-// import { Card, CardHeader, CardContent, CardMedia, Avatar, Typography, IconButton } from '@mui/material';
-// import { ThumbUp, Share } from '@mui/icons-material';
-
-// export default function Post({ post }) {
-//   return (
-//     <Card sx={{ marginBottom: 2 }}>
-//       <CardHeader
-//         avatar={<Avatar src={post.author.avatar} />}
-//         title={post.author.name}
-//         subheader={post.date}
-//       />
-//       <CardMedia
-//         component="img"
-//         image={post.image}
-//         alt={post.title}
-//       />
-//       <CardContent>
-//         <Typography variant="h6">{post.title}</Typography>
-//         <Typography variant="body2">{post.text}</Typography>
-//         <div>
-//           <IconButton>
-//             <ThumbUp /> {post.likes}
-//           </IconButton>
-//           <IconButton>
-//             <Share /> {post.shares}
-//           </IconButton>
-//         </div>
-//       </CardContent>
-//     </Card>
-//   );
-// }
-
 import React from 'react';
 import { Card, CardHeader, CardContent, CardMedia, Avatar, Typography, IconButton, List, ListItem, ListItemText } from '@mui/material';
 import { ThumbUp, Share } from '@mui/icons-material';
