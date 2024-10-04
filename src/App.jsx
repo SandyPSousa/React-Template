8ad9a9b3b5614f06913cd040aee1f07403f8e9cb
Sandy <sandy.sousa@al.infnet.edu.br>
2024-10-04 16:24:08 -0300

    teste para o vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import './App.css'
 
 export default function App() {
+    const openPage = (page) => {
+        window.open(`src/TP1/${page}/index.html`, '_blank');
+    };
+
     return (
         <main>
-            <a href="src/TP1/01/index.html">TP 1.1</a>
-            <a href="src/TP1/02/index.html">TP 1.2</a>
-            <a href="src/TP1/03/index.html">TP 1.3</a>
-            <a href="src/TP1/04/index.html">TP 1.4</a>
-            <a href="src/TP1/05/index.html">TP 1.5</a>
-            <a href="src/TP1/06/index.html">TP 1.6</a>
-            <a href="src/TP1/07/index.html">TP 1.7</a>
-            <a href="src/TP1/08/index.html">TP 1.8</a>
-            <a href="src/TP1/09/index.html">TP 1.9</a>
-            <a href="src/TP1/10/index.html">TP 1.10</a>
-            <a href="src/TP1/11/index.html">TP 1.11</a>
-            <a href="src/TP1/12/index.html">TP 1.12</a>
-            <a href="src/TP1/13/index.html">TP 1.13</a>
-            <a href="src/TP1/14/index.html">TP 1.14</a>
-            <a href="src/TP1/15/index.html">TP 1.15</a>
-            <a href="src/TP1/16/index.html">TP 1.16</a>
+            <button onClick={() => openPage('01')}>TP 1.1</button>
+            <button onClick={() => openPage('02')}>TP 1.2</button>
+            <button onClick={() => openPage('03')}>TP 1.3</button>
+            <button onClick={() => openPage('04')}>TP 1.4</button>
+            <button onClick={() => openPage('05')}>TP 1.5</button>
+            <button onClick={() => openPage('06')}>TP 1.6</button>
+            <button onClick={() => openPage('07')}>TP 1.7</button>
+            <button onClick={() => openPage('08')}>TP 1.8</button>
+            <button onClick={() => openPage('09')}>TP 1.9</button>
+            <button onClick={() => openPage('10')}>TP 1.10</button>
+            <button onClick={() => openPage('11')}>TP 1.11</button>
+            <button onClick={() => openPage('12')}>TP 1.12</button>
+            <button onClick={() => openPage('13')}>TP 1.13</button>
+            <button onClick={() => openPage('14')}>TP 1.14</button>
+            <button onClick={() => openPage('15')}>TP 1.15</button>
+            <button onClick={() => openPage('16')}>TP 1.16</button>
         </main>
-    )
+    );
 }
